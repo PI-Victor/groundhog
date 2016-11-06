f0d638db5c98dd4ce03a945bbfc9826e56734a52
Victor Palade <palade.ionut@gmail.com>
2016-11-06 21:25:59 +0100

    add a delegator and a controller for handling directory walks

--- pkg/system/system.go
+++ pkg/system/system.go
@@ -6,15 +6,32 @@ import (
 	"os"
 	"path"
 	"path/filepath"
+	"strings"
+)
+
+const (
+	jpeg = "jpeg"
+	jpg  = "jpg"
+	bmp  = "bmp"
+	png  = "png"
+)
+
+var (
+	// okFormats are the accepted formats for the files that we want to rename.
+	okFormats = []string{jpeg, jpg, bmp, png}
 )
 
 func validatePath(localPath string) error {
 	if localPath == "" {
-		return errors.New("Path sent as argument is empty!")
+		return errors.New("Path is empty!")
 	}
 	if !path.IsAbs(localPath) {
 		return errors.New("Path is not absolute!")
 	}
+	if _, err := os.Stat(localPath); err != nil {
+		return err
+	}
+
 	return nil
 }
 
@@ -27,17 +44,46 @@ func Run(localPath string) {
 	if err != nil {
 		log.Fatalf("An error occured while reading the %s directory: %s", localPath, err)
 	}
-	filepath.Walk(dir, printDir)
+	if err = controller(dir); err != nil {
+		log.Fatal(err)
+	}
 }
 
-func printDir(path string, info os.FileInfo, err error) error {
-	log.Println(path)
-	log.Println(info)
+// controller dispatches a new delegator in a goroutine for each directory/file
+// it encounters.
+func controller(dir string) error {
+	// i want a channel that returns errors to the controller, up the stack, from
+	// the spawned delegators. a new delegator is spawned only if the file is a
+	// directory.
+
+	//errOutput := make(chan error)
+	go filepath.Walk(dir, delegator)
 	return nil
 }
 
-// makeBackup creates a backup of the file, before renaming it and storing it
-// in the directory specified.
-func makeBackup() error {
+// delegator is called for each file in the specified directory and analyses if
+// the file is a simple file or a directory. If it is a directory it will call
+// itself recursively and spawn a new delegator.
+func delegator(path string, info os.FileInfo, err error) error {
+	fileMode := info.Mode()
+	if fileMode.IsDir() {
+		go filepath.Walk(path, delegator)
+	}
+
+	if fileMode.IsRegular() && validateExt(info.Name()) {
+		return nil
+	}
 	return nil
+}
+
+// validateExtension checks if the file contains a valid extention.
+func validateExt(fileName string) bool {
+	fileExt := fileName[strings.LastIndex(fileName, ".")+1:]
+	for _, format := range okFormats {
+		if fileExt != format {
+			continue
+		}
+		return false
+	}
+	return true
 }
